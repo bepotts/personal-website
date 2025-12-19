8ef3a2ec1351d196789a5917e51c0ebc0de0e707
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-12-19 10:37:29 -0500

    added more styling to the main section

--- components/layout/main/Main.tsx
+++ components/layout/main/Main.tsx
@@ -4,10 +4,15 @@ import './main.css'
 function MainSection() {
     return(
         <main className="main-section">
-            <h1>Hello!</h1>
-            <h2>Welcome to the website of Brandon Potts!</h2>
-            <p>I&apos;m a software developer who enjoys to be build things</p>
-            <p>If you&apos;d like to contact me, feel free to <a href="mailto:bepotts47@gmail.com?subject=Contact%20from%20your%20website">contact me</a></p>
+            <div className="intro-section">
+                <h1>Hello!</h1>
+                <h2>Welcome to the website of Brandon Potts!</h2>
+            </div>
+            
+            <div className="info-section">
+                <p>I&apos;m a software developer who enjoys to be build things</p>
+                <p>If you&apos;d like to contact me, feel free to <a href="mailto:bepotts47@gmail.com?subject=Contact%20from%20your%20website">contact me</a></p>
+            </div>
         </main>
     )
 }
