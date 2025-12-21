f251f481a16e2315379b8a12cb45ae3fe02bf15b
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-12-21 12:27:15 -0500

    removed sticky header

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 function Header() {
   return (
-    <header className="bg-blue-500 sticky top-0">
+    <header className="bg-blue-500 top-0">
       <div className="mx-auto px-8 py-4 bg-green-500 flex justify-center">
         <Image src="/bp-logo.svg" alt="BP logo" width={100} height={20} />
       </div>
