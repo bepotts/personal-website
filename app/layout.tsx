352ace426089523ff868b7d72889f1aeb538d1e4
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-12-23 12:11:19 -0500

    fixed bg naming convention editior issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-fluid-base bg-[var(--background)] text-[var(--foreground)] font-sans`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-fluid-base bg-background text-foreground font-sans`}
       >
         {children}
       </body>
