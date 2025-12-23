9154be75f5d79c5007c7bc2ffb28a5df2d7aad5d
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-12-23 11:59:20 -0500

    removed "any" edititor error in Header and Main

--- components/layout/main/Main.test.tsx
+++ components/layout/main/Main.test.tsx
@@ -4,7 +4,7 @@ import MainSection from './Main';
 // Mock next/image
 jest.mock('next/image', () => ({
   __esModule: true,
-  default: (props: any) => {
+  default: (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
     // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
     return <img {...props} />;
   },
